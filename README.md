19799ac977e2f1f206f4e07084874452f3b10ec7
Rafael Amorim <3620428+rafaelmamorim@users.noreply.github.com>
2022-01-24 10:46:29 -0300

    Fixed bug: Post selector appear in user wall #2

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ This component allows users to decide to see posts from all or friends only by s
 
 ![Screenshot](https://www.rafaelamorim.com.br/temp/homepageposts.png)
 
+## Known issue
+
+If any other component uses the ossn_get_homepage_wall_access() function it is necessary to change it to hpage_posts_get_homepage_wall_access() for everything to work correctly. As far as I know, this function is used in the following components:
+
+- [HashTag](https://www.openteknik.com/product/view/2355/hash-tags>)
+- [PopularPosts](https://www.openteknik.com/product/view/4869/popularposts)¹. 
+
+¹ Thanks to [Hugo](https://www.opensource-socialnetwork.org/u/Erassus) for testing and reporting
+
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
--- plugins/default/js/homepageposts.php
+++ plugins/default/js/homepageposts.php
@@ -14,20 +14,23 @@
 
 $(document).ready(function () {
 
-    var WallSelector = '<div class="ossn-wall-access" style="border-bottom-right-radius: 10px;border-bottom-left-radius: 10px;">';
-    WallSelector += '    <div class="ossn-widget">';
-    WallSelector += '        <div class="widget-heading container-fluid">';
-    WallSelector += '            <div class="row" >'; 
-    WallSelector += '                <div class="col text-center">';
-    WallSelector += '                    <a href="<?php echo ossn_site_url('public'); ?>"  class="<?php echo OssnSession::getSession('com_wall_type_access')=='public' ?  ' text-danger' :''; ?>"><?php echo ossn_print('homepageposts:option:public');?></a>';
-    WallSelector += '                </div>';
-    WallSelector += '                <div class="col text-center  <?php echo OssnSession::getSession('com_wall_type_access')=='friends' ?  'text-danger' :''; ?>">';
-    WallSelector += '                    <a href="<?php echo ossn_site_url('friends'); ?>"  class="<?php echo OssnSession::getSession('com_wall_type_access')=='friends' ?  ' text-danger"' :''; ?>"><?php echo ossn_print('homepageposts:option:friends');?></a>';
-    WallSelector += '                </div>';
-    WallSelector += '            </div>';
-    WallSelector += '        </div>';
-    WallSelector += '    </div>';
-    WallSelector += '</div>';
-    $(WallSelector).insertAfter('.ossn-wall-container');
+    var $url = window.location.href;
+        if (!$url.match('/u/')) {
+            var WallSelector = '<div class="ossn-wall-access" style="border-bottom-right-radius: 10px;border-bottom-left-radius: 10px;">';
+            WallSelector += '    <div class="ossn-widget">';
+            WallSelector += '        <div class="widget-heading container-fluid">';
+            WallSelector += '            <div class="row" >'; 
+            WallSelector += '                <div class="col text-center">';
+            WallSelector += '                    <a href="<?php echo ossn_site_url('public'); ?>"  class="<?php echo OssnSession::getSession('com_wall_type_access')=='public' ?  ' text-danger' :''; ?>"><?php echo ossn_print('homepageposts:option:public');?></a>';
+            WallSelector += '                </div>';
+            WallSelector += '                <div class="col text-center  <?php echo OssnSession::getSession('com_wall_type_access')=='friends' ?  'text-danger' :''; ?>">';
+            WallSelector += '                    <a href="<?php echo ossn_site_url('friends'); ?>"  class="<?php echo OssnSession::getSession('com_wall_type_access')=='friends' ?  ' text-danger"' :''; ?>"><?php echo ossn_print('homepageposts:option:friends');?></a>';
+            WallSelector += '                </div>';
+            WallSelector += '            </div>';
+            WallSelector += '        </div>';
+            WallSelector += '    </div>';
+            WallSelector += '</div>';
+            $(WallSelector).insertAfter('.ossn-wall-container');
+        }
 
 });
